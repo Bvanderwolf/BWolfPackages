24086947632b25b0473c836ef1ea17c5ded9033d
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-06-13 16:40:58 +0200

    Add stat modification info to represent a stat modifier

---
+++ Assets/BWolf/Examples/StatModification/StatUsingComponent.cs
@@ -0,0 +1,11 @@
+﻿using BWolf.Utilities.StatModification;
+using UnityEngine;
+
+namespace BWolf.Examples.StatModification
+{
+    public class StatUsingComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private StatModifierInfo statModifier;
+    }
+}
---
+++ Assets/BWolf/Utilities/StatModification/StatModifierInfo.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+namespace BWolf.Utilities.StatModification
+{
+    [System.Serializable]
+    public struct StatModifierInfo
+    {
+        [Tooltip("Identifier for stat modifier")]
+        public string Name;
+
+        [Tooltip("Amount of value it will modify")]
+        public int Value;
+
+        [Tooltip("Can this modifier stack with modifiers with the same name?")]
+        public bool CanStack;
+
+        [Tooltip("Will this modifier increase or subtract")]
+        public bool Increase;
+
+        [Tooltip("Will this modifier modify current value or max value?")]
+        public bool ModifiesCurrent;
+
+        [Tooltip("Does this modifier, when max is modified, also modify current?")]
+        public bool ModifiesCurrentWithMax;
+
+        public static StatModifierInfo Create(string name, int value, bool increase)
+        {
+            StatModifierInfo info;
+            info.Name = name;
+            info.Value = value;
+            info.CanStack = false;
+            info.Increase = increase;
+            info.ModifiesCurrent = true;
+            info.ModifiesCurrentWithMax = false;
+            return info;
+        }
+
+        public static StatModifierInfo Create(string name, int value, bool increase, bool canStack)
+        {
+            StatModifierInfo info;
+            info.Name = name;
+            info.Value = value;
+            info.CanStack = canStack;
+            info.Increase = increase;
+            info.ModifiesCurrent = true;
+            info.ModifiesCurrentWithMax = false;
+            return info;
+        }
+
+        public static StatModifierInfo Create(string name, int value, bool increase, bool canStack, bool modifiesCurrent)
+        {
+            StatModifierInfo info;
+            info.Name = name;
+            info.Value = value;
+            info.CanStack = canStack;
+            info.Increase = increase;
+            info.ModifiesCurrent = modifiesCurrent;
+            info.ModifiesCurrentWithMax = false;
+            return info;
+        }
+
+        public static StatModifierInfo Create(string name, int value, bool increase, bool canStack, bool modifiesCurrent, bool modifiesCurrentWithMax)
+        {
+            StatModifierInfo info;
+            info.Name = name;
+            info.Value = value;
+            info.CanStack = canStack;
+            info.Increase = increase;
+            info.ModifiesCurrent = modifiesCurrent;
+            info.ModifiesCurrentWithMax = modifiesCurrentWithMax;
+            return info;
+        }
+    }
+}
