469e4810182c7dbfe4af3722bec22d3d4c3095d6
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-06-22 11:05:16 +0200

    Finish basic functionalities and calbacks for creating a room

--- Assets/BWolf/Examples/PhotonWrapper/Scripts/CreateRoomForm.cs
+++ Assets/BWolf/Examples/PhotonWrapper/Scripts/CreateRoomForm.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace BWolf.Examples.PhotonWrapper
@@ -20,22 +21,43 @@ namespace BWolf.Examples.PhotonWrapper
         private string nameEntered;
         private string passwordEntered;
 
+        private bool nameWasFocused;
+        private bool passwordWasFocused;
+
         private void Awake()
         {
             btnFinish.onClick.AddListener(OnFinishButtonClick);
+            inputName.onEndEdit.AddListener(OnNameEntered);
+            inputPassword.onEndEdit.AddListener(OnPasswordEntered);
             CheckFinishability();
+        }
+
+        private void OnEnable()
+        {
+            EventSystem.current.SetSelectedGameObject(inputName.gameObject);
         }
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Tab))
+            if (Input.GetKeyDown(KeyCode.Tab) && nameWasFocused)
             {
+                EventSystem.current.SetSelectedGameObject(inputPassword.gameObject);
             }
+
+            if (Input.GetKeyDown(KeyCode.Return) && (nameWasFocused || passwordWasFocused))
+            {
+                TryReturnInputFinish();
+            }
+
+            nameWasFocused = inputName.isFocused;
+            passwordWasFocused = inputPassword.isFocused;
         }
 
         private void OnDestroy()
         {
             btnFinish.onClick.RemoveListener(OnFinishButtonClick);
+            inputName.onEndEdit.RemoveListener(OnNameEntered);
+            inputPassword.onEndEdit.RemoveListener(OnPasswordEntered);
         }
 
         public void OnNameEntered(string edit)
@@ -52,6 +74,14 @@ namespace BWolf.Examples.PhotonWrapper
         public void OnFinishButtonClick()
         {
             controller.CreateRoomFinish(nameEntered, passwordEntered);
+        }
+
+        private void TryReturnInputFinish()
+        {
+            if (!string.IsNullOrEmpty(inputName.text))
+            {
+                controller.CreateRoomFinish(inputName.text, passwordEntered);
+            }
         }
 
         private void CheckFinishability()
