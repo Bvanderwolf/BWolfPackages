c24250effa0d2fb9f7f7a1f5479021a983ee2794
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-06-24 17:28:52 +0200

    Add summaries to new functionalities

--- Assets/BWolf/Examples/PhotonWrapper/Scripts/NicknameForm.cs
+++ Assets/BWolf/Examples/PhotonWrapper/Scripts/NicknameForm.cs
@@ -30,18 +30,21 @@ namespace BWolf.Examples.PhotonWrapper
             nicknameWasFocused = inputNickname.isFocused;
         }
 
+        /// <summary>Actives this form by setting it active, focusing on nickname and storing the finish action</summary>
         public void Activate(Action<string> finishAction)
         {
             gameObject.SetActive(true);
-            this.finishAction = finishAction;
             EventSystem.current.SetSelectedGameObject(inputNickname.gameObject);
+            this.finishAction = finishAction;
         }
 
+        /// <summary>Cancels this form by setting it inactive</summary>
         public void OnCancel()
         {
             gameObject.SetActive(false);
         }
 
+        /// <summary>Finishes the form by firing the finish action event using the written nickname as argument if it is a valid name</summary>
         public void OnFinish()
         {
             string name = inputNickname.text;
