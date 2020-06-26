76c4d0f145596f080eeb07e4fa2dc21cf40bbd8c
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-06-26 21:58:17 +0200

    Add materials for disks and crosses to be used by player color properties

---
+++ Assets/BWolf/Examples/PhotonWrapper/Scripts/Game/GameAssetHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace BWolf.Examples.PhotonWrapper.Game
+{
+    public class GameAssetHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private Material diskMaterial = null;
+
+        [SerializeField]
+        private Material crossMaterial = null;
+
+        private void Awake()
+        {
+            diskMaterial.color = Color.blue;
+            crossMaterial.color = Color.red;
+        }
+    }
+}
