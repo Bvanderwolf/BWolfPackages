8cee9aca19310a0da0ce4ddcad44e411d092652f
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-06-30 19:50:12 +0200

    Add turn management scripts

---
+++ Assets/BWolf/Examples/PhotonWrapper/Scripts/Game/TurnExtensions.cs
@@ -0,0 +1,9 @@
+﻿using BWolf.Wrappers.PhotonSDK;
+
+public static class TurnExtensions
+{
+    public static bool HasTurn(this Client client)
+    {
+        return true;
+    }
+}
---
+++ Assets/BWolf/Examples/PhotonWrapper/Scripts/Game/TurnManager.cs
@@ -0,0 +1,25 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace BWolf.Examples.PhotonWrapper.Game
+{
+    public class TurnManager : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text playerOneHead = null;
+
+        [SerializeField]
+        private TMP_Text playerTwoHead = null;
+
+        [SerializeField]
+        private float playerTurnOutlineWidth = 0.25f;
+
+        private float defaultOutlineWidth;
+
+        private void Awake()
+        {
+            defaultOutlineWidth = playerOneHead.outlineWidth;
+            playerOneHead.outlineWidth = playerTurnOutlineWidth;
+        }
+    }
+}
