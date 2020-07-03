7761d74bc52f9f5e98633ed3c44a57ae2f51ab69
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-07-03 11:50:22 +0200

    Add template code for unit groups and formations

---
+++ Assets/BWolf/Utilities/SquadFormations/UnitFormation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BWolf.Utilities.SquadFormations
+{
+    public class UnitFormation
+    {
+        private Vector3 orientation;
+        private State state;
+        private int unitCount;
+
+        private List<Vector3> positions;
+        private List<Vector3> orientations;
+
+        private enum State
+        {
+            Broken,
+            Forming,
+            Formed
+        }
+    }
+}
---
+++ Assets/BWolf/Utilities/SquadFormations/UnitGroup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BWolf.Utilities.SquadFormations
+{
+    public class UnitGroup
+    {
+        private int groupId;
+        private int formationId;
+        private int unitCount;
+        private float maxSpeed;
+
+        private List<GameObject> units;
+        private List<Vector3> desiredPositions;
+        private GameObject centroid;
+        private GameObject commander;
+
+        public void AddUnit(GameObject unit)
+        {
+        }
+
+        public void RemoveUnit(GameObject unit)
+        {
+        }
+    }
+}
