763042511786525bb4270557750a4df4f4a264e0
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-07-03 10:27:30 +0200

    Add squadformations demo scene with simple camera controller

---
+++ Assets/BWolf/Examples/SquadFormations/Scripts/CameraController.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+
+namespace BWolf.Examples.SquadFormations
+{
+    public class CameraController : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField]
+        private float panSpeed = 0.3f;
+
+        [SerializeField]
+        private float panSpeedMultiplier = 5;
+
+        [SerializeField]
+        private float camDistanceMin = 10f;
+
+        [SerializeField]
+        private float camDistanceMax = 50f;
+
+        [SerializeField]
+        private float camDistanceDefault = 30f;
+
+        [SerializeField]
+        private float camDistanceDelta = 3f;
+
+        [SerializeField]
+        private float camTiltMin = 30f;
+
+        [SerializeField]
+        private float camTiltMax = 60f;
+
+        [SerializeField]
+        private float camTiltDefault = 50f;
+
+        [SerializeField]
+        private float camMouseTiltSensitivity = 2f;
+
+        [SerializeField]
+        private float camRotationDelta = 2f;
+
+        [SerializeField]
+        private float camMouseRotationSensitivity = 2f;
+
+        private float camAngle = 0f;
+        private float camRotation = 0f;
+        private CameraInput input;
+
+        public float CamDistanceMin
+        {
+            get { return camDistanceMin; }
+        }
+
+        public float CamDistanceMax
+        {
+            get { return camDistanceMax; }
+        }
+
+        public float CamDistance { get; private set; } = 0f;
+
+        private void Start()
+        {
+            CamDistance = camDistanceDefault;
+            camAngle = camTiltDefault;
+            input = GetComponent<CameraInput>();
+        }
+
+        private void FixedUpdate()
+        {
+            Rotate(input.Rotation * camRotationDelta);
+
+            Pan(input.PanDirection);
+        }
+
+        private void Update()
+        {
+            Scroll(input.ScrollDelta * camDistanceDelta);
+
+            Rotate(input.MouseRotation * camMouseRotationSensitivity);
+
+            Tilt(input.MouseTilt * camMouseTiltSensitivity);
+        }
+
+        /// <summary>Change camera distance from centerpoint with given delta</summary>
+        private void Scroll(float distanceDelta)
+        {
+            CamDistance += distanceDelta;
+            CamDistance = Mathf.Clamp(CamDistance, camDistanceMin, camDistanceMax);
+        }
+
+        /// <summary>Change camera rotation around centerpoint with given delta</summary>
+        private void Rotate(float rotationDelta)
+        {
+            camRotation += rotationDelta;
+            transform.rotation = Quaternion.Euler(0f, camRotation, 0f);
+        }
+
+        /// <summary>Change camera tilt (= angle between centerpoint and camera) with given delta </summary>
+        private void Tilt(float angleDelta)
+        {
+            camAngle += angleDelta;
+            camAngle = Mathf.Clamp(camAngle, camTiltMin, camTiltMax);
+        }
+
+        /// <summary>Move camera centerpoint position in given direction</summary>
+        private void Pan(Vector3 direction)
+        {
+            transform.Translate(direction * panSpeed * (input.PanBoost ? panSpeedMultiplier : 1), Space.Self);
+        }
+    }
+}
---
+++ Assets/BWolf/Examples/SquadFormations/Scripts/CameraInput.cs
@@ -0,0 +1,215 @@
+﻿using UnityEngine;
+
+namespace BWolf.Examples.SquadFormations
+{
+    public class CameraInput : MonoBehaviour
+    {
+        [Header("Settings")]
+        [SerializeField]
+        private bool enableArrowsPanning = true;
+
+        [SerializeField]
+        private bool enableWASDPanning = true;
+
+        [SerializeField]
+        private bool enableEdgePanning = false;
+
+        [SerializeField]
+        private bool enableEdgePanningInEditor = false;
+
+        [SerializeField]
+        private bool enableQERotation = true;
+
+        [SerializeField]
+        private bool enableMouse3Rotation = true;
+
+        [SerializeField]
+        private bool enableMouse3Tilt = true;
+
+        [SerializeField]
+        private bool invertQERotation = false;
+
+        [SerializeField]
+        private bool invertMouse3Rotation = false;
+
+        [SerializeField]
+        private bool invertMouse3Tilt = false;
+
+        [Header("References")]
+        [SerializeField]
+        private Camera cam = null;
+
+        public Vector3 PanDirection { get; private set; }
+        public bool PanBoost { get; private set; }
+        public int ScrollDelta { get; private set; }
+        public float Rotation { get; private set; }
+        public float MouseRotation { get; private set; }
+        public float MouseTilt { get; private set; }
+        public bool GoToNearestUnit { get; private set; }
+
+        private void Update()
+        {
+            PanDirection = GetPanDirection();
+
+            PanBoost = Input.GetKey(KeyCode.LeftShift);
+
+            ScrollDelta = GetScrollDelta();
+
+            Rotation = GetRotation();
+
+            MouseRotation = GetMouseRotation();
+
+            MouseTilt = GetMouseTilt();
+
+            GoToNearestUnit = Input.GetKey(KeyCode.Space);
+        }
+
+        /// <summary>Return the panning direction based on enabled input</summary>
+        private Vector3 GetPanDirection()
+        {
+            Vector3 panDirection = Vector3.zero;
+
+            if (enableArrowsPanning)
+            {
+                if (Input.GetKey(KeyCode.RightArrow))
+                {
+                    panDirection += Vector3.right;
+                }
+
+                if (Input.GetKey(KeyCode.LeftArrow))
+                {
+                    panDirection += Vector3.left;
+                }
+
+                if (Input.GetKey(KeyCode.UpArrow))
+                {
+                    panDirection += Vector3.forward;
+                }
+
+                if (Input.GetKey(KeyCode.DownArrow))
+                {
+                    panDirection += Vector3.back;
+                }
+            }
+
+            if (enableWASDPanning)
+            {
+                if (Input.GetKey(KeyCode.D))
+                {
+                    panDirection += Vector3.right;
+                }
+
+                if (Input.GetKey(KeyCode.A))
+                {
+                    panDirection += Vector3.left;
+                }
+
+                if (Input.GetKey(KeyCode.W))
+                {
+                    panDirection += Vector3.forward;
+                }
+
+                if (Input.GetKey(KeyCode.S))
+                {
+                    panDirection += Vector3.back;
+                }
+            }
+
+            if (Application.isEditor ? enableEdgePanningInEditor : enableEdgePanning)
+            {
+                if (cam.ScreenToViewportPoint(Input.mousePosition).x > 1)
+                {
+                    panDirection += Vector3.right;
+                }
+
+                if (cam.ScreenToViewportPoint(Input.mousePosition).x < 0)
+                {
+                    panDirection += Vector3.left;
+                }
+
+                if (cam.ScreenToViewportPoint(Input.mousePosition).y > 1)
+                {
+                    panDirection += Vector3.forward;
+                }
+
+                if (cam.ScreenToViewportPoint(Input.mousePosition).y < 0)
+                {
+                    panDirection += Vector3.back;
+                }
+            }
+
+            return panDirection;
+        }
+
+        /// <summary>Return the scroll delta input</summary>
+        private int GetScrollDelta()
+        {
+            int scrollDelta = 0;
+
+            if (Input.mouseScrollDelta.y > 0)
+            {
+                scrollDelta = -1;
+            }
+
+            if (Input.mouseScrollDelta.y < 0)
+            {
+                scrollDelta = 1;
+            }
+
+            return scrollDelta;
+        }
+
+        /// <summary>Return the rotation input</summary>
+        private float GetRotation()
+        {
+            float rotation = 0;
+
+            if (enableQERotation)
+            {
+                if (Input.GetKey(KeyCode.Q))
+                {
+                    rotation += (invertQERotation ? -1 : 1);
+                }
+
+                if (Input.GetKey(KeyCode.E))
+                {
+                    rotation += (invertQERotation ? 1 : -1);
+                }
+            }
+
+            return rotation;
+        }
+
+        /// <summary>Return the mouse rotation input</summary>
+        private float GetMouseRotation()
+        {
+            float mouseRotation = 0;
+
+            if (enableMouse3Rotation)
+            {
+                if (Input.GetMouseButton(2))
+                {
+                    mouseRotation += Input.GetAxis("Mouse X") * (invertMouse3Rotation ? -1 : 1);
+                }
+            }
+
+            return mouseRotation;
+        }
+
+        /// <summary>Return the mouse tilt input</summary>
+        private float GetMouseTilt()
+        {
+            float mouseTilt = 0;
+
+            if (enableMouse3Tilt)
+            {
+                if (Input.GetMouseButton(2))
+                {
+                    mouseTilt += Input.GetAxis("Mouse Y") * (invertMouse3Tilt ? 1 : -1);
+                }
+            }
+
+            return mouseTilt;
+        }
+    }
+}
