782714845b15d5ed76a66d7070fe907d6120aca3
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-07-05 22:19:05 +0200

    Add start on unit group information canvas

---
+++ Assets/BWolf/Examples/SquadFormations/Scripts/UnitGroupInfo.cs
@@ -0,0 +1,28 @@
+﻿using BWolf.Utilities.SquadFormations.Units;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BWolf.Examples.SquadFormations
+{
+    public class UnitGroupInfo : MonoBehaviour
+    {
+        [SerializeField]
+        private Text txtId = null;
+
+        [SerializeField]
+        private Text txtUnitCount = null;
+
+        [SerializeField]
+        private Text txtFormation = null;
+
+        [SerializeField]
+        private Text txtCommander = null;
+
+        private UnitGroup groupToDisplay;
+
+        public void AssignGroupToDisplay(UnitGroup group)
+        {
+            groupToDisplay = group;
+        }
+    }
+}
---
+++ Assets/BWolf/Examples/SquadFormations/Scripts/UnitGroupInfoHandler.cs
@@ -0,0 +1,26 @@
+﻿using BWolf.Utilities.SquadFormations.Units;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BWolf.Examples.SquadFormations
+{
+    public class UnitGroupInfoHandler : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject prefabUnitGroupInfo = null;
+
+        private List<UnitGroupInfo> infoComponents = new List<UnitGroupInfo>();
+        private List<UnitGroup> groupsToDisplay = new List<UnitGroup>();
+
+        private int groupCount = 0;
+
+        private void Update()
+        {
+            if (UnitGroupHandler.GetGroup(groupCount) != null)
+            {
+                groupsToDisplay.Add(UnitGroupHandler.GetGroup(groupCount));
+                groupCount++;
+            }
+        }
+    }
+}
