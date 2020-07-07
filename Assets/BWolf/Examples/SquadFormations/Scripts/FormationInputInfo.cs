38a30a8fee7008b80cfad26e277e7ae0a03c00fe
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-07-07 13:40:45 +0200

    Add formation info in screen for changing formation

---
+++ Assets/BWolf/Examples/SquadFormations/Scripts/FormationInputInfo.cs
@@ -0,0 +1,45 @@
+﻿using BWolf.Utilities.SquadFormations.Units;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace BWolf.Examples.SquadFormations
+{
+    public class FormationInputInfo : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text txtName = null;
+
+        [SerializeField]
+        private TMP_Text txtKey = null;
+
+        private KeyCode input;
+        private string nameOfSetting;
+
+        private bool setup = false;
+
+        /// <summary>Setups up formation input info with name of setting and key to activate it</summary>
+        public void Setup(string name, KeyCode key)
+        {
+            nameOfSetting = name;
+            input = key;
+
+            txtName.text = name;
+            txtKey.text = key.ToString();
+
+            setup = true;
+        }
+
+        private void Update()
+        {
+            if (setup && Input.GetKeyDown(input))
+            {
+                List<UnitGroup> selectedGroups = UnitGroupHandler.GetSelectedGroups();
+                foreach (UnitGroup group in selectedGroups)
+                {
+                    group.Formation.UpdateSetting(nameOfSetting);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/BWolf/Examples/SquadFormations/Scripts/UnitFormationInfo.cs
@@ -0,0 +1,40 @@
+﻿using BWolf.Utilities.SquadFormations.Units;
+using UnityEngine;
+
+namespace BWolf.Examples.SquadFormations
+{
+    public class UnitFormationInfo : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject prefabFormationInput = null;
+
+        [SerializeField]
+        private Transform layoutTransform = null;
+
+        private int groupGrab;
+
+        private void Update()
+        {
+            if (groupGrab == -1) { return; }
+
+            if (UnitGroupHandler.GetGroup(groupGrab) != null)
+            {
+                CreateFormationInput();
+                groupGrab = -1;
+            }
+        }
+
+        /// <summary>creates formation input info for each stored formation setting</summary>
+        private void CreateFormationInput()
+        {
+            int startingKey = (int)KeyCode.Alpha1;
+            UnitGroup group = UnitGroupHandler.GetGroup(groupGrab);
+            foreach (FormationSetting setting in group.Formation.StoredSettings)
+            {
+                KeyCode key = (KeyCode)startingKey;
+                Instantiate(prefabFormationInput, layoutTransform).GetComponent<FormationInputInfo>().Setup(setting.Name, key);
+                startingKey++;
+            }
+        }
+    }
+}
