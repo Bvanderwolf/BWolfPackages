b2ff7fe0f87fe8ef5c8b14ad66346bd523066e24
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-07-30 12:22:15 +0200

    Add singleton and lerp value utility scripts

---
+++ Assets/BWolf/Utilities/LerpValue/LerpValue.cs
@@ -0,0 +1,48 @@
+﻿namespace BWolf.Utilities.LerpValue
+{
+    /// <summary>Representation of lerp values</summary>
+    public struct LerpValue<T> where T : struct
+    {
+        public readonly T Start;
+        public readonly T End;
+        public readonly float Time;
+
+        private float currentTime;
+
+        public float Perc
+        {
+            get
+            {
+                return currentTime / Time;
+            }
+        }
+
+        public LerpValue(T start, T end, float time, float currentTime = 0)
+        {
+            Start = start;
+            End = end;
+            Time = time;
+
+            this.currentTime = currentTime;
+        }
+
+        /// <summary>
+        /// Returns if current time has reached time value. Increments current time by t if this is not the case
+        /// </summary>
+        public bool IncrementTime(float t)
+        {
+            if (currentTime == Time)
+            {
+                return true;
+            }
+
+            currentTime += t;
+            if (currentTime > Time)
+            {
+                currentTime = Time;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Assets/BWolf/Utilities/SingletonBehaviour/SingletonBehaviour.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+
+namespace BWolf.Utilities.SingletonBehaviour
+{
+    /// <summary>
+    /// A singleton class to be derived from by monobehaviours to make use of the singleton functionality but keep monobehaviour functionalities like coroutines
+    /// </summary>
+    public class SingletonBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private readonly static Lazy<T> lazyInstance = new Lazy<T>(CreateSingleton);
+
+        private static bool appIsQuitting;
+
+        public static T Instance
+        {
+            get
+            {
+                if (appIsQuitting)
+                {
+                    //if this object is already destroyed during app closage return null to avoid MissingReferenceException
+                    return null;
+                }
+
+                return lazyInstance.Value;
+            }
+        }
+
+        //make sure no instance of this object can be made using new keyword by making the constructor protected
+        protected SingletonBehaviour()
+        {
+        }
+
+        protected virtual void OnDestroy()
+        {
+            appIsQuitting = true;
+        }
+
+        /// <summary>Returns singleton instance by first searching object of type T in scene and otherwise creating one if not was found</summary>
+        private static T CreateSingleton()
+        {
+            var search = FindObjectOfType<T>();
+            if (search != null)
+            {
+                return search;
+            }
+            else
+            {
+                var gameObject = new GameObject($"{typeof(T).Name} (singleton)");
+                var instance = gameObject.AddComponent<T>();
+                DontDestroyOnLoad(gameObject);
+                return instance;
+            }
+        }
+    }
+}
