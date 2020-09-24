83ad60245b68cc199a0b8a708b36b26ea0406342
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-09-24 10:47:41 +0200

    Add start on SceneTransitioning package

---
+++ Assets/BWolf/Examples/SceneTransitioning/SceneSwitchButton.cs
@@ -0,0 +1,20 @@
+﻿using BWolf.Utilities.SceneTransitioning;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BWolf.Examples.SceneTransitioning
+{
+    public class SceneSwitchButton : MonoBehaviour
+    {
+        [SerializeField]
+        private string nameOfScene = string.Empty;
+
+        private void Awake()
+        {
+            GetComponent<Button>().onClick.AddListener(() =>
+            {
+                SceneTransitionSystem.Instance.Transition(nameOfScene);
+            });
+        }
+    }
+}
---
+++ Assets/BWolf/Utilities/SceneTransitioning/SceneTransitionSystem.cs
@@ -0,0 +1,19 @@
+﻿using BWolf.Behaviours;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+namespace BWolf.Utilities.SceneTransitioning
+{
+    public class SceneTransitionSystem : LazySingletonBehaviour<SceneTransitionSystem>
+    {
+        public void Transition(int sceneIndex)
+        {
+            SceneManager.LoadScene(sceneIndex);
+        }
+
+        public void Transition(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
