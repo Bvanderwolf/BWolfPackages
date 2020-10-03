a8741bb395c1ef1a954932e61bfe4428a491f113
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-10-03 20:01:17 +0200

    Remove depricated OnProgressUpdate delegate

--- Assets/BWolf/Utilities/SceneTransitioning/OnProgressUpdate.cs
+++
@@ -1,9 +0,0 @@
-﻿// Created By: Benjamin van der Wolf @ https://bvanderwolf.github.io/
-// Version: 1.0
-//----------------------------------
-
-namespace BWolf.Utilities.SceneTransitioning
-{
-    /// <summary>Called when a SceneTransition its progress has been updated.</summary>
-    public delegate void OnProgressUpdate(float percentage);
-}
