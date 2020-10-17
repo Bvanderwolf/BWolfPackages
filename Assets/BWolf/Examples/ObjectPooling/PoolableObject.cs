fd8ba9217e1f82bde5ef875d3a3059846ed43297
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2020-10-17 12:55:23 +0200

    Add Generic ObjectPooler script and an Example scene to test it in

---
+++ Assets/BWolf/Examples/ObjectPooling/PoolableObject.cs
@@ -0,0 +1,23 @@
+﻿using BWolf.Utilities.ObjectPooling;
+using UnityEngine;
+
+namespace BWolf.Examples.ObjectPooling
+{
+    public class PoolableObject : MonoBehaviour, IPoolable
+    {
+        public bool CanTake
+        {
+            get { return !gameObject.activeInHierarchy; }
+        }
+
+        public void Return()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void OnTake()
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/BWolf/Utilities/ObjectPooling/IPoolable.cs
@@ -0,0 +1,11 @@
+﻿namespace BWolf.Utilities.ObjectPooling
+{
+    public interface IPoolable
+    {
+        bool CanTake { get; }
+
+        void Return();
+
+        void OnTake();
+    }
+}
