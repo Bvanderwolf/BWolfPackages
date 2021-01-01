cbe547b584dc6a154a6f2f6d6d01061315bc565d
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2021-01-01 15:17:55 +0100

    Add Example scene for FileStorage Demo

---
+++ Assets/BWolf/Examples/FileStorage/InputStorageUI.cs
@@ -0,0 +1,31 @@
+﻿using BWolf.Utilities.FileStorage;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BWolf.Examples.FileStorage
+{
+    public class InputStorageUI : MonoBehaviour
+    {
+        [SerializeField]
+        private InputField input = null;
+
+        private const string FILE_PATH = "inputSave.txt";
+
+        public void OnSaveButtonClick()
+        {
+            string text = input.text;
+            if (!string.IsNullOrEmpty(text))
+            {
+                FileStorageSystem.SaveAsPlainText(FILE_PATH, text, SaveMode.Hashed);
+            }
+        }
+
+        public void OnLoadButtonClick()
+        {
+            if (FileStorageSystem.LoadFromPlainText<string>(FILE_PATH, out LoadResult<string> loadResult))
+            {
+                input.text = loadResult.data;
+            }
+        }
+    }
+}
