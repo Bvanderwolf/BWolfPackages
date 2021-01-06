baeb2bdc8e320351151db9f4e93f64346a130d11
B.van.der.Wolf <benjamin.van.der.wolf@hva.nl>
2021-01-06 15:42:16 +0100

    Split File Storage System up in Save and Load parts using partial keyword

---
+++ Assets/BWolf/Utilities/FileStorage/FileStorageLoadPart.cs
@@ -0,0 +1,112 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+namespace BWolf.Utilities.FileStorage
+{
+    public static partial class FileStorageSystem
+    {
+        /// <summary>Tries loading and outputting serialized value at given path from the root directory</summary>
+        public static bool LoadFromFile<T>(string path, out T outValue)
+        {
+            string filePath = Path.Combine(rootPath, path);
+            if (!File.Exists(filePath))
+            {
+                outValue = default;
+                return false;
+            }
+
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(filePath, FileMode.Open);
+
+            outValue = (T)bf.Deserialize(file);
+            file.Close();
+
+            return true;
+        }
+
+        /// <summary>Loads json string at given path from root directory and converts it into T. Can also be used for loading monobehaviours and scriptableobjects</summary>
+        public static bool LoadJsonFromFile<T>(string path, out LoadResult<T> loadResult) where T : new()
+        {
+            string filePath = Path.Combine(rootPath, path);
+            if (!File.Exists(filePath))
+            {
+                loadResult.data = default;
+                loadResult.result = LoadResult.FileNotFound;
+                return false;
+            }
+            else
+            {
+                T data = new T();
+                string json = File.ReadAllText(filePath);
+                JsonUtility.FromJsonOverwrite(json, data);
+
+                loadResult.data = data;
+
+                string key = $"{typeof(T).Name}@{path}";
+                if (PlayerPrefs.HasKey(key))
+                {
+                    string storedHash = PlayerPrefs.GetString(key);
+                    string loadedHash = EncryptionSystem.Hash(json);
+                    if (storedHash != loadedHash)
+                    {
+                        Debug.LogWarning($"Loaded data {loadResult.data} @ {filePath} path isn't the same as the saved data :: this is not intended");
+                        loadResult.result = LoadResult.FileChanged;
+                        return true;
+                    }
+                }
+
+                loadResult.result = LoadResult.Succesfull;
+                return true;
+            }
+        }
+
+        /// <summary>Loads data of <typeparamref name="T"/> as a string from given path</summary>
+        /// <typeparam name="T">The type of object to load from text</typeparam>
+        public static bool LoadFromPlainText<T>(string path, out LoadResult<string> loadResult)
+        {
+            string filePath = Path.Combine(rootPath, path);
+
+            if (!File.Exists(filePath))
+            {
+                loadResult.data = string.Empty;
+                loadResult.result = LoadResult.FileNotFound;
+                return false;
+            }
+            else
+            {
+                loadResult.data = File.ReadAllText(filePath);
+
+                string key = $"{typeof(T).Name}@{path}";
+                if (PlayerPrefs.HasKey(key))
+                {
+                    //if a hash was stored for this loadable object in player prefs, check if it matches the loaded one
+                    string storedHash = PlayerPrefs.GetString(key);
+                    string loadedHash = EncryptionSystem.Hash(loadResult.data);
+                    if (storedHash != loadedHash)
+                    {
+                        Debug.LogWarning($"Loaded data {loadResult.data} @ {filePath} path isn't the same as the saved data :: this is not intended");
+                        loadResult.result = LoadResult.FileChanged;
+                        return true;
+                    }
+                }
+
+                loadResult.result = LoadResult.Succesfull;
+                return true;
+            }
+        }
+    }
+
+    public struct LoadResult<T>
+    {
+        public LoadResult result;
+        public T data;
+    }
+
+    public enum LoadResult
+    {
+        FileNotFound = 0,
+        FileChanged,
+        Succesfull
+    }
+}
--- Assets/BWolf/Utilities/FileStorage/FileStorageSavePart.cs
+++ Assets/BWolf/Utilities/FileStorage/FileStorageSavePart.cs
@@ -1,5 +1,5 @@
 ﻿// Created By: Benjamin van der Wolf @ https://bvanderwolf.github.io/
-// Version: 1.1
+// Version: 1.2
 //----------------------------------
 
 using System.IO;
@@ -10,7 +10,7 @@ using UnityEngine;
 namespace BWolf.Utilities.FileStorage
 {
     /// <summary>Basic file storage system that can handle saving and loading data including monobehaviours and scriptableobjects</summary>
-    public static class FileStorageSystem
+    public static partial class FileStorageSystem
     {
         private static readonly string rootPath;
         private static readonly object saveLock;
@@ -44,25 +44,6 @@ namespace BWolf.Utilities.FileStorage
             await Task.Run(() => SaveToFile(path, value));
         }
 
-        /// <summary>Tries loading and outputting serialized value at given path from the root directory</summary>
-        public static bool LoadFromFile<T>(string path, out T outValue)
-        {
-            string filePath = Path.Combine(rootPath, path);
-            if (!File.Exists(filePath))
-            {
-                outValue = default;
-                return false;
-            }
-
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(filePath, FileMode.Open);
-
-            outValue = (T)bf.Deserialize(file);
-            file.Close();
-
-            return true;
-        }
-
         /// <summary>Saves value as json string at given path from root directory. Can also be used for saving monobehaviours and scriptableobjects</summary>
         public static void SaveAsJsonToFile<T>(string path, T value, SaveMode mode = SaveMode.UnSafe)
         {
@@ -89,42 +70,6 @@ namespace BWolf.Utilities.FileStorage
         public static async void SaveAsJsonToFileASync<T>(string path, T value, SaveMode mode = SaveMode.UnSafe)
         {
             await Task.Run(() => SaveAsJsonToFile(path, value, mode));
-        }
-
-        /// <summary>Loads json string at given path from root directory and converts it into T. Can also be used for loading monobehaviours and scriptableobjects</summary>
-        public static bool LoadJsonFromFile<T>(string path, out LoadResult<T> loadResult) where T : new()
-        {
-            string filePath = Path.Combine(rootPath, path);
-            if (!File.Exists(filePath))
-            {
-                loadResult.data = default;
-                loadResult.result = LoadResult.FileNotFound;
-                return false;
-            }
-            else
-            {
-                T data = new T();
-                string json = File.ReadAllText(filePath);
-                JsonUtility.FromJsonOverwrite(json, data);
-
-                loadResult.data = data;
-
-                string key = $"{typeof(T).Name}@{path}";
-                if (PlayerPrefs.HasKey(key))
-                {
-                    string storedHash = PlayerPrefs.GetString(key);
-                    string loadedHash = EncryptionSystem.Hash(json);
-                    if (storedHash != loadedHash)
-                    {
-                        Debug.LogWarning($"Loaded data {loadResult.data} @ {filePath} path isn't the same as the saved data :: this is not intended");
-                        loadResult.result = LoadResult.FileChanged;
-                        return true;
-                    }
-                }
-
-                loadResult.result = LoadResult.Succesfull;
-                return true;
-            }
         }
 
         /// <summary>Saves the given data as plain text at given path</summary>
@@ -155,41 +100,6 @@ namespace BWolf.Utilities.FileStorage
             await Task.Run(() => SaveAsPlainText(path, data, mode));
         }
 
-        /// <summary>Loads data of <typeparamref name="T"/> as a string from given path</summary>
-        /// <typeparam name="T">The type of object to load from text</typeparam>
-        public static bool LoadFromPlainText<T>(string path, out LoadResult<string> loadResult)
-        {
-            string filePath = Path.Combine(rootPath, path);
-
-            if (!File.Exists(filePath))
-            {
-                loadResult.data = string.Empty;
-                loadResult.result = LoadResult.FileNotFound;
-                return false;
-            }
-            else
-            {
-                loadResult.data = File.ReadAllText(filePath);
-
-                string key = $"{typeof(T).Name}@{path}";
-                if (PlayerPrefs.HasKey(key))
-                {
-                    //if a hash was stored for this loadable object in player prefs, check if it matches the loaded one
-                    string storedHash = PlayerPrefs.GetString(key);
-                    string loadedHash = EncryptionSystem.Hash(loadResult.data);
-                    if (storedHash != loadedHash)
-                    {
-                        Debug.LogWarning($"Loaded data {loadResult.data} @ {filePath} path isn't the same as the saved data :: this is not intended");
-                        loadResult.result = LoadResult.FileChanged;
-                        return true;
-                    }
-                }
-
-                loadResult.result = LoadResult.Succesfull;
-                return true;
-            }
-        }
-
         /// <summary>Checks whether the file at given filePath its parent directory exists. Creates it if doesn't</summary>
         private static void VerifyFileDirectoryPath(string filePath)
         {
@@ -199,19 +109,6 @@ namespace BWolf.Utilities.FileStorage
                 Directory.CreateDirectory(directoryPath);
             }
         }
-    }
-
-    public struct LoadResult<T>
-    {
-        public LoadResult result;
-        public T data;
-    }
-
-    public enum LoadResult
-    {
-        FileNotFound = 0,
-        FileChanged,
-        Succesfull
     }
 
     public enum SaveMode
