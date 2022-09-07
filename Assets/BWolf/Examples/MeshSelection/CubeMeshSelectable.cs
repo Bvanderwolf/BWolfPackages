74f692f231ac68f8a500ef6ea3ae3ded91b1c60b
Benjamin <benjamin.vd.wolf@movares.nl>
2022-09-07 15:44:43 +0200

    add summaries and finish up selection

---
+++ Assets/BWolf/Examples/MeshSelection/CubeMeshSelectable.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace BWolf.MeshSelecting
+{
+    [RequireComponent(typeof(BoxCollider))]
+    public class CubeMeshSelectable : MonoBehaviour, ISelectableMesh
+    {
+        private MeshRenderer _renderer;
+
+        private Color _color;
+
+        private void Awake()
+        {
+            _renderer = GetComponent<MeshRenderer>();
+            _color = _renderer.material.color;
+        }
+
+        public void OnClick()
+        {
+            Debug.Log($"{name} is clicked");
+        }
+
+        public void OnSelect()
+        {
+            _renderer.material.color = Color.blue;
+        }
+
+        public void OnDeselect()
+        {
+            _renderer.material.color = _color;
+        }
+    }
+}
